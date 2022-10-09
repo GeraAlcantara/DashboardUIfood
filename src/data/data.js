5786cbaf664bd6884c9c8cb5b48641f59165803e
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-10-08 20:30:27 -0500

    search and pills

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+import { FaShoppingCart, FaHome, FaBell, FaThLarge, FaClock } from "react-icons/fa";
+import { HiChartPie } from "react-icons/hi";
+import { BsGearFill, BsFillBellFill } from "react-icons/bs";
+
+export const Navitems = [
+  {
+    name: "Home",
+    Icon: FaHome,
+  },
+  {
+    name: "Table",
+    Icon: FaBell,
+  },
+  {
+    name: "Order",
+    Icon: FaShoppingCart,
+  },
+  {
+    name: "History",
+    Icon: FaClock,
+  },
+  {
+    name: "Report",
+    Icon: HiChartPie,
+  },
+  {
+    name: "Alert",
+    Icon: BsFillBellFill,
+  },
+  {
+    name: "Settings",
+    Icon: BsGearFill,
+  },
+];
